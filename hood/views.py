e1511c63a252ca897109914eefabc2bbdefb8cf0
Dean Kago <martokhago@gmail.com>
2022-06-20 01:42:45 +0300

    created register user function in views

--- hood/views.py
+++ hood/views.py
@@ -4,6 +4,23 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 
+
+def register(request):
+  if request.method == 'POST':
+    form = SignupForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password1')
+      user = authenticate(username=username, password=password)
+      login(request, user)
+      return redirect('index')
+  else:
+    form = SignupForm()
+  return render(request, 'registration/register.html', {form: form})
+
+
+
 def index(request):
   return render(request, 'main/index.html')
 
