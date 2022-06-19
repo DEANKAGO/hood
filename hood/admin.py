76a27a323bcd6fef765db34bbcf35d5ac01ca944
Dean Kago <martokhago@gmail.com>
2022-06-19 17:44:56 +0300

    created index view

--- hood/admin.py
+++ hood/admin.py
@@ -6,3 +6,4 @@ from .models import *
 admin.site.register(Neighborhood),
 admin.site.register(Profile),
 admin.site.register(Business),
+admin.site.register(Post),
--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def index(request):
+  return render(request, 'main/index.html')
