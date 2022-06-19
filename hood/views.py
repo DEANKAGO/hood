7a44288ff58b1e0b1035ca3d9d79649a667a4352
Dean Kago <martokhago@gmail.com>
2022-06-19 23:07:20 +0300

    added leave hood views function

--- hood/views.py
+++ hood/views.py
@@ -68,3 +68,8 @@ def join_hood(request, id):
   return redirect('hood')
 
 
+def leave_hood(request, id):
+  hood = get_object_or_404(Neighbourhood, id=id)
+  request.user.profile.neighbourhood = None
+  request.user.profile.save()
+  return redirect('hood')
