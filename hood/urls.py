2afdca562613e122fcdd96f09a58571523754d78
Dean Kago <martokhago@gmail.com>
2022-06-19 19:09:53 +0300

    created single hood view

--- hood/urls.py
+++ hood/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('all-hoods/', views.all_hoods, name='hood'),
 ]
 
 if settings.DEBUG:
--- hood/views.py
+++ hood/views.py
@@ -12,3 +12,28 @@ def all_hoods(request):
   all_hoods = Neighborhood.objects.all()
   all_hoods = all_hoods[::-1]
   return render(request, 'main/all_hoods.html', locals())
+
+
+
+def single_hood(request, hood_id):
+  hood = Neighborhood.objects.get(id=hood_id)
+  business = Business.objects.filter(neighborhood=hood)
+  posts = Post.objects.filter(hood=hood)
+  posts = posts[::-1]
+  if request.method == 'POST':
+    form = BusinessForm(request.POST)
+    if form.is_valid():
+      business_form = form.save(commit=False)
+      business_form.neighborhood = hood 
+      business_form.user = request.user.profile
+      business_form.save()
+      return render('main/single_hood.html', hood.id)
+  else:
+    form = BusinessForm()
+  context = {
+    'hood': hood,
+    'business': business,
+    'form': form,
+    'posts': posts,
+  }
+  return render(request, 'main/single_hood.html', context)
