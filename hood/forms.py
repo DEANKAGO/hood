03e2ad79295f88c23410de4252008443c7977010
Dean Kago <martokhago@gmail.com>
2022-06-20 01:52:44 +0300

    added signup form

--- hood/forms.py
+++ hood/forms.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
+class SignupForm(UserCreationForm):
+  email = forms.EmailField(max_length=200, help_text='Required, Input a valid email address.')
+
+  class Meta:
+    model = User
+    fields = ('username','email', 'password1', 'password2')
+
+
 class BusinessForm(forms.ModelForm):
   class Meta:
     model = Business
@@ -14,7 +22,7 @@ class BusinessForm(forms.ModelForm):
 class NeighbourhoodForm(forms.ModelForm):
   class Meta:
     model = Neighbourhood
-    exclude = ('admin')
+    exclude = ('admin',)
 
 
 class UpdateProfileForm(forms.ModelForm):
