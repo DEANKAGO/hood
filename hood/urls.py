4fda0af5b9aef998e9b5dca63d9a67f27a71169b
Dean Kago <martokhago@gmail.com>
2022-06-19 23:21:18 +0300

    added profile url path

--- hood/urls.py
+++ hood/urls.py
@@ -6,12 +6,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('profile/<username>', views.profile, name='profile'),
   path('all_hoods/', views.all_hoods, name='hood'),
   path('single_hood/<hood_id>', views.single_hood, name='single_hood'),
   path('new_hood/', views.create_hood, name='new_hood'),
   path('<hood_id>/members', views.members, name='members'),
   path('join_hood/<id>', views.join_hood, name='join_hood'),
   path('leave_hood/<id>', views.leave_hood, name='leave_hood'),
+
 ]
 
 if settings.DEBUG:
